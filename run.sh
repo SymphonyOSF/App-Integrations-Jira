939850cc0865adeeffe1d3b06f843132c756f667
Maurizio Pillitu <maoo@apache.org>
2017-07-04 18:09:20 +0200

    added support for openshift online

--- run.sh
+++ run.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
 
-# Run integrations locally
+#
+# Run integrations locally or on Openshift
+#
+
+# Name of the spring-boot application to run, see src/main/resources/application-jira.yml
+APP_ID=jira
 
 # Import environment variables
-. ./env.sh
+if [ -f ./env.sh ]; then
+  . ./env.sh
+fi
 
-# Build the project and the spring boot bundle
-mvn clean install -Prun
+if [[ -d /opt/openshift ]]; then
+  echo "run.sh - Running on Openshift"
+  INTEGRATION_JAR=./integration-jira-1.2.0-SNAPSHOT.jar
+  LOG_BASEDIR=.
+  JAVA_CMD_OPTS="-Xmx350m"
+  YAML_TEMPLATE=./application.yaml.template
+  cd /opt/openshift
+  mkdir ./certs
+  cp -s /tmp/*.p12 ./certs
+else
+  echo "run.sh - Running locally"
+  INTEGRATION_JAR=target/bundle/integration-jira-1.2.0-SNAPSHOT.jar
+  LOG_BASEDIR=target
+  JAVA_CMD_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,address=5000,suspend=n
+  YAML_TEMPLATE=./target/bundle/application.yaml.template
+  mvn clean install -Prun,bundle
+fi
 
 # Inject environment variables in application.yaml
+# TODO - use environment vars syntax in application.yaml and skip this step
+echo "Generating application.yaml file"
 rm -rf application.yaml
-curl -s https://raw.githubusercontent.com/symphonyoss/contrib-toolbox/master/scripts/inject-vars.sh | bash -s -- ./local-run/application.yaml.template application.yaml
+curl -s https://raw.githubusercontent.com/symphonyoss/contrib-toolbox/master/scripts/inject-vars.sh | bash -s -- $YAML_TEMPLATE application.yaml
 
 # Cleanup tomcat folder from previous runs
 rm -rf tomcat ; mkdir tomcat
 
+# Install and start ngrok
+# echo "Installing ngrok"
+# curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+# unzip -o ngrok-stable-linux-amd64.zip
+# chmod +x ngrok
+# echo "Running ngrok from folder $PWD"
+# ./ngrok authtoken $NGROK_TOKEN
+# ./ngrok http --subdomain hubspot.symphonyoss --log=stdout 8186 > ngrok.log &
+
+# echo "Checking that ngrok is running..."
+# sleep 3
+# ps auxwww | grep ngrok
+# echo "ngrok logs..."
+# cat ngrok.log
+
 # Run the Spring Boot application
-java -Dlog4j2.outputAllToConsole=true -Dlogs.basedir=target \
--agentlib:jdwp=transport=dt_socket,server=y,address=5000,suspend=n \
--jar target/integration.jar \
---server.tomcat.basedir=$PWD/tomcat
+echo "Running Spring Boot app from folder $PWD:"
+java -Dlog4j2.outputAllToConsole=true -Dlogs.basedir=$LOG_BASEDIR $JAVA_CMD_OPTS \
+-jar $INTEGRATION_JAR \
+--spring.profiles.active=$APP_ID \
+--server.tomcat.basedir=$PWD/tomcat \
+--server.address=0.0.0.0
